2d62224dee7ed0645b8ec26301dabc8d98ac5ea2
Biencredible <fabian.bien@gmx.de>
2023-01-16 10:07:57 +0100

    Working With Strings

---
+++ WorkingWithStrings.py
@@ -0,0 +1,13 @@
+phrase = "Giraffe Academy"
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.upper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase[3])
+print(phrase.index("G"))
+print(phrase.index("a"))
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
+
