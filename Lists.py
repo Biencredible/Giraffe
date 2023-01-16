defdcdde4f19da87f4c62708ea893df7901280d4
Biencredible <fabian.bien@gmx.de>
2023-01-16 14:10:09 +0100

    Lists

---
+++ Lists.py
@@ -0,0 +1,12 @@
+#Eckige Klammer sind Liste
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+print(friends)
+print(friends[1])
+#Negative fangen von hinten an zu zählen
+print(friends[-1])
+print(friends[1:])
+print(friends[1:3])
+print(friends[:3])
+friends[1] = "Mike"
+print(friends[1])
+
