1a185750bd48458d8881d3f9269d8ac8f08826e9
Biencredible <fabian.bien@gmx.de>
2023-01-16 11:15:47 +0100

    Working with numbers

---
+++ WorkingWithNumbers.py
@@ -0,0 +1,19 @@
+from math import * #include
+print(2)
+print(2.087)
+print(-2.087)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+print(10 % 3)
+my_num = 5
+print(str(my_num) + " my favorite number")
+print(abs(-my_num))
+print(pow(3, 2))
+print(max(4, 6))
+print(min(4, 6))
+print(round(4.6))
+print(round(4.5)) #Python rounds down .5
+print(round(3.2))
+print(floor(8.9))
+print(ceil(8.1))
+print(sqrt(36))
