2ac76182189d51c576975797f1688f2ab7360bf7
Biencredible <fabian.bien@gmx.de>
2023-01-16 09:26:28 +0100

    Variables & Datatypes

---
+++ app.py
@@ -0,0 +1,9 @@
+character_name = "Tom"
+character_age = "18"
+is_male = True
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Bob"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ". ")
