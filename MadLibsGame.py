9b6b79dbb7b307b07982ba221521723c26eadcab
Biencredible <fabian.bien@gmx.de>
2023-01-16 13:52:06 +0100

    Mad Libs Game

---
+++ MadLibsGame.py
@@ -0,0 +1,7 @@
+color = input("Enter color: ")
+plural_noun = input("Enter plural noun: ")
+celebrity = input("Enter celebrity: ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
