31e56971e81ed029ea94a4ebcde0b11e78f5d747
Biencredible <fabian.bien@gmx.de>
2023-01-17 11:29:01 +0100

    If Statements

---
+++ IfStatements.py
@@ -0,0 +1,12 @@
+
+is_male = False
+is_tall = True
+
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male")
+elif not(is_male) and is_tall:
+    print("You are a not a male but are tall")
+else:
+    print("Your are neither male nor tall")
