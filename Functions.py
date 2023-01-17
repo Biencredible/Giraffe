d36de0842f31d321a82854cf7c80a828fb864dca
Biencredible <fabian.bien@gmx.de>
2023-01-17 10:05:35 +0100

    Functions

---
+++ Functions.py
@@ -0,0 +1,10 @@
+def say_hi(name):
+    print("Hi " + name)
+
+say_hi("Mike")
+say_hi("Steve")
+
+def say_hi_to_name_with_age(name, age):
+    print("Hi " + name + ", you are " + str(age))
+
+say_hi_to_name_with_age("Steve", 25)
