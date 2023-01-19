459922ef92690153308cb66f74b71f3f4ee5de40
Biencredible <fabian.bien@gmx.de>
2023-01-19 15:20:17 +0100

    Object Functions

---
+++ ObjectFunctions.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+student1 = Student("Tim", "VWL", 3.1, False)
+student2 = Student("Kate", "Law", 3.8, True)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
